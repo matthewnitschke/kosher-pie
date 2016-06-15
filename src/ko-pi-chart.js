f01a62da91218255f4c88dd200f6d986ae0205cd
Matthew Nitschke <mathuc33@gmail.com>
2016-06-14 19:57:43 -0600

    Initial Implementation

--- src/ko-pi-chart.js
+++ src/ko-pi-chart.js
@@ -1,6 +1,19 @@
 ko.components.register("pi-chart", {
   viewModel: function(params){
 
+    self.percentage = params.percentage;
+
+    self.size = params.size;
+
+    self.r = ko.computed(function(){ return ko.unwrap(self.size) / 2 });
+    self.c = ko.computed(function(){ return ko.unwrap(self.size) / 2 });
+
+    self.b = ko.computed(function(){ return 2 * Math.PI * self.r() });
+
+    self.a = ko.computed(function(){ return self.b() * (ko.unwrap(self.percentage) * .01) });
+
+
+    self.strokeWidth = ko.computed(function(){return ko.unwrap(self.size) });
   },
-  template: {element: ''}
-})
+  template: {element: 'pi-chart-template'}
+});
