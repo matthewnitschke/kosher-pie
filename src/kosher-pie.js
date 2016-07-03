d0ccf47fb7f9127465800e49ae3cd5aed494cb9f
Matthew Nitschke <mathuc33@gmail.com>
2016-07-03 17:27:30 -0600

    Complete rework of everything
    Multi slices now available

--- src/kosher-pie.js
+++ src/kosher-pie.js
@@ -1,77 +1,56 @@
-ko.components.register("kosher-pie", {
+function Circle(data){
+  var self = this;
+
+  self.color = data.color;
+  self.percent = data.percent;
+  self.offset = data.offset;
+}
+
+ko.components.register('kosher-pie', {
   viewModel: function(params){
 
-    var defaults = {
-      fill: "#ddd",
-      colors: ["#0074d9"]
+    var data = params.data;
+    var colors = params.colors
+
+    self.circles = ko.observableArray();
+
+    self.backgroundColor = ko.computed(function(){
+      if (self.circles().length > 0){
+        return self.circles()[self.circles().length-1].color;
+      } else {
+        return "#FFF";
+      }
+    });
+
+    var sumOfData = ko.computed(function(){
+      var total = 0;
+
+      ko.unwrap(data).forEach(function(item){
+        total+=item;
+      });
+
+      return total;
+    });
+
+    var offset = 0;
+    for (var i = 0; i < data.length; i ++){
+      var perc = getCorrectedPerc(data[i], sumOfData());
+
+      self.circles.push(new Circle({
+        color: colors[i],
+        percent: perc,
+        offset: offset
+      }));
+
+      offset += perc;
     }
 
-    var options = extend(defaults, params);
 
-
-    self.data = observifyArray(options.data);
-    self.size = observify(options.size);
-
-    self.dataSum = ko.computed(function(){
-      var sum = 0;
-      for(var i = 0; i <self.data().length; i ++){
-        sum += parseInt(self.data()[i]());
-      }
-      return sum;
-    });
-
-
-    self.r = ko.computed(function(){ return self.size() / 2 });
-    self.strokeWidth = ko.computed(function(){ return self.size() });
-
-    self.b = ko.computed(function(){ return 2 * Math.PI * self.r() });
-    self.a = ko.computed(function(){
-      return (self.b() * self.data()[1]()) / self.dataSum();
-    });
-
-    self.fill = options.fill;
-    self.stroke = options.colors[0];
-
-
-    function extend(a, b) {
-         for (var key in b){
-             if (b.hasOwnProperty(key)){
-                   a[key] = b[key];
-             }
-        }
-         return a;
-     }
-
-     function observifyArray(arr){
-       for(var i = 0; i < ko.unwrap(arr).length; i ++){
-         ko.unwrap(arr)[i] = observify(ko.unwrap(arr)[i]);
-       }
-       if (ko.isObservable(arr) && 'push' in arr){
-         return arr;
-       } else {
-         return ko.observableArray(arr);
-       }
-     }
-
-     function observify(value){
-       if (ko.isObservable(value)){
-         return value;
-       } else {
-         return ko.observable(value);
-       }
-     }
+    function getCorrectedPerc(val, sum){
+      return (val * 100) / sum;
+    }
   },
-  template: "<div class=\"kosher-pie__wrapper\">\
-               <svg data-bind=\"attr: {width: ko.unwrap(size), height: ko.unwrap(size) }\" class=\"kosher-pie__\">\
-                 <circle class=\"kosher-pie__\" data-bind=\"attr: {\
-                   r: r(),\
-                   cx: r(),\
-                   cy: r(),\
-                   style: 'stroke-dasharray: ' + a() + ' ' + b() +\
-                          ';stroke-width: ' + strokeWidth() +\
-                          ';fill: ' + ko.unwrap(fill) +\
-                          ';stroke: ' + ko.unwrap(stroke)\
-                 }\"></circle>\
-               </svg>\
-             </div>"
-});
+  template: '<svg viewBox="0 0 32 32" data-bind="foreach: circles, attr: {style: \'background-color: \' + backgroundColor()}">\
+			<circle r="16" cx="16" cy="16" data-bind="attr: { stroke: color, \'stroke-dasharray\': percent + \' 100\', \'stroke-dashoffset\': \'-\' + offset }"></circle>\
+		</svg>'
+})
